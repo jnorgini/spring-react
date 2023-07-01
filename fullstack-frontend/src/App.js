3ce8e34d89093bcb0cf76026580175259f59db14
jnorgini <jnorgini@gmail.com>
2023-07-01 20:31:01 -0300

    database update

--- fullstack-frontend/src/App.js
+++ fullstack-frontend/src/App.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AddUser from './users/AddUser';
+import AddUser from './users/AddUser'
 
 function App() {
   return (
